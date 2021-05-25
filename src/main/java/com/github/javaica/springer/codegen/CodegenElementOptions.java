7b1af8ada5df31490a2049b0c99f3833c6421567
Ilya Kuznetsov <ikuz2002@gmail.com>
2021-05-25 13:54:34 +0300

    chore: Remove useless imports

--- src/main/java/com/github/javaica/springer/codegen/CodegenElementOptions.java
+++ src/main/java/com/github/javaica/springer/codegen/CodegenElementOptions.java
@@ -4,8 +4,6 @@ import com.github.javaica.springer.model.CodegenElementType;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiDirectory;
-import com.intellij.psi.PsiFile;
-import com.intellij.psi.PsiPackage;
 import lombok.Builder;
 import lombok.Value;
 
